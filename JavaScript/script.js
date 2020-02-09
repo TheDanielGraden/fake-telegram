1be2082ad4c1ca897130b89bb6e4b4a1a93c87e7
TheDanielGraden <danielgraden505@gmail.com>
2020-02-09 22:58:57 +0200

    update

---
+++ JavaScript/script.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+var dayarray=new Array("Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота")
+function getthedate(){
+var mydate=new Date()
+mydate=new Date(mydate.getUTCFullYear(), mydate.getUTCMonth(), mydate.getUTCDate(), mydate.getUTCHours(), mydate.getUTCMinutes(), mydate.getUTCSeconds())
+var year=mydate.getYear()
+if (year < 1000)
+year+=1900
+var day=mydate.getDay()
+var month=mydate.getMonth()
+var daym=mydate.getDate()
+if (daym<10)
+daym="0"+daym
+month=month+1
+if (month<10)
+month="0"+month
+var hours=mydate.getHours()
+var minutes=mydate.getMinutes()
+var seconds=mydate.getSeconds()
+if (hours<=9)
+hours="0"+hours
+if (minutes<=9)
+minutes="0"+minutes
+if (seconds<=9)
+seconds="0"+seconds
+//change font size here
+var cdate=hours+":"+minutes+":"+seconds+" " + dayarray[day]+" "+daym+"."+month+"."+year+" "
+if (document.all)
+document.all.clock.innerHTML=cdate
+else if (document.getElementById)
+document.getElementById("clock").innerHTML=cdate
+else
+document.write(cdate)
+}
+if (!document.all&&!document.getElementById)
+getthedate()
+function goforit(){
+if (document.all||document.getElementById)
+setInterval("getthedate()",1000)
+}
